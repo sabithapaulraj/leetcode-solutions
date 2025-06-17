8939aedb84cd9adf4bb9f75fb6e4ecf7b8b83489
Sabitha Paulraj <118343379+sabithapaulraj@users.noreply.github.com>
2025-06-17 22:27:20 +0530

    Create 344-ReverseString

---
+++ 344-ReverseString/344-ReverseString.py
@@ -0,0 +1,8 @@
+# Last updated: 6/17/2025, 10:27:21 PM
+class Solution:
+    def reverseString(self, s):
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
