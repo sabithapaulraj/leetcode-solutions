86d6c749d25d67e33b91dd9c9751348078540de9
Sabitha Paulraj <118343379+sabithapaulraj@users.noreply.github.com>
2025-06-17 22:27:30 +0530

    Create 238-ProductOfArrayExceptSelf

---
+++ 238-ProductOfArrayExceptSelf/238-ProductOfArrayExceptSelf.py
@@ -0,0 +1,13 @@
+# Last updated: 6/17/2025, 10:27:30 PM
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        answer = [1] * (len(nums))
+        prefix = 1
+        for i in range(len(nums)):
+            answer[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) -1, -1, -1):
+            answer[i] *= postfix
+            postfix *= nums[i]
+        return answer
