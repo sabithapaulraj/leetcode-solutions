39635ae2051f38816af123d85d778c997f19b7a7
Sabitha Paulraj <118343379+sabithapaulraj@users.noreply.github.com>
2025-06-18 12:04:33 +0530

    Create 1365-HowManyNumbersAreSmallerThantheCurrentNumber

---
+++ 1365-HowManyNumbersAreSmallerThantheCurrentNumber/version/java/1365-HowManyNumbersAreSmallerThantheCurrentNumber_20250618_120433.java
@@ -0,0 +1,20 @@
+// Last updated: 6/18/2025, 12:04:33 PM
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        for(int i = 0; i < n; i++){
+            int count = 0;
+            for(int j = 0; j < n; j++){
+                if((j != i) && nums[j] < nums[i]){
+                    count++;
+                }
+            }
+            result[i] = count;
+        }
+        return result;
+
+        
+    }
+}
