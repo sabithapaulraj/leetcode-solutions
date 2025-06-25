aaf88d46d698fe4479766262e2146e6d8a48bd75
Sabitha Paulraj <118343379+sabithapaulraj@users.noreply.github.com>
2025-06-25 22:50:06 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/version/java/242-ValidAnagram_20250625_225006.java
@@ -0,0 +1,25 @@
+// Last updated: 6/25/2025, 10:50:06 PM
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> counter = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            counter.put(ch, counter.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            if (!counter.containsKey(ch) || counter.get(ch) == 0) {
+                return false;
+            }
+            counter.put(ch, counter.get(ch) - 1);
+        }
+
+        return true;        
+    }
+}
