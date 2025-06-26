7deddeaf2ca10fe94ebb43df2ef06c7330913c91
Sabitha Paulraj <118343379+sabithapaulraj@users.noreply.github.com>
2025-06-26 20:35:53 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/version/java/70-ClimbingStairs_20250626_203553.java
@@ -0,0 +1,16 @@
+// Last updated: 6/26/2025, 8:35:53 PM
+class Solution {
+    public int climbStairs(int n) {
+        if(n == 1 || n == 2){
+            return n;
+        }
+        int prev1 = 1;
+        int prev2 = 2;
+        for(int i = 3; i <= n; i++){
+            int current = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = current;
+        }
+        return prev2;
+    }
+}
