c0899242d979888e642b36a4224d33bac7aba1fa
Sabitha Paulraj <118343379+sabithapaulraj@users.noreply.github.com>
2025-06-28 10:55:36 +0530

    Create 204-CountPrimes

---
+++ 204-CountPrimes/version/java/204-CountPrimes_20250628_105535.java
@@ -0,0 +1,32 @@
+// Last updated: 6/28/2025, 10:55:35 AM
+class Solution {
+    public int countPrimes(int n) {
+        if(n <= 2) return 0;
+        if(n == 3) return 1;
+
+        //Initialize Boolean Array full of True
+        boolean[] isPrime = new boolean[n];
+        // Set all to true
+        for(int i = 2; i < n; i++){
+            isPrime[i] = true;
+        }
+
+        // Traverse Again
+        for(int i = 2; i * i < n; i++){ // Square i
+            if(isPrime[i]){ // If true, in this case all are true
+                for(int j = i * i; j < n; j+=i ) {// Skip i items at a time
+                    isPrime[j] = false; // Set to false
+            }
+            }
+        }
+
+            // Count Prime Numbers 
+            int count = 0;
+            for(int i = 0; i<n; i++){
+                if(isPrime[i]){
+                    count++;
+                }
+            }
+        return count;
+    }
+}
