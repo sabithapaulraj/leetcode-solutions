158e1dd377ef73962268af6228623c7d52e17a31
Sabitha Paulraj <118343379+sabithapaulraj@users.noreply.github.com>
2025-06-30 19:26:30 +0530

    Create 42-TrappingRainWater

---
+++ 42-TrappingRainWater/version/java/42-TrappingRainWater_20250630_192631.java
@@ -0,0 +1,22 @@
+// Last updated: 6/30/2025, 7:26:31 PM
+class Solution {
+    public int trap(int[] height) {
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0, water = 0;
+
+        while (left < right) {
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+
+            if (leftMax < rightMax) {
+                water += leftMax - height[left];
+                left++;
+            } else {
+                water += rightMax - height[right];
+                right--;
+            }
+        }
+
+        return water;
+    }
+}
