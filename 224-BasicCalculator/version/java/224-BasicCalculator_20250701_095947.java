0071cff7c17ad9be4fb29e67de697555a7e9e430
Sabitha Paulraj <118343379+sabithapaulraj@users.noreply.github.com>
2025-07-01 09:59:48 +0530

    Create 224-BasicCalculator

---
+++ 224-BasicCalculator/version/java/224-BasicCalculator_20250701_095947.java
@@ -0,0 +1,42 @@
+// Last updated: 7/1/2025, 9:59:47 AM
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int result = 0;
+        int number = 0;
+        int sign = 1;
+        for(int i = 0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(Character.isDigit(c)){
+                number = number * 10 + (int)(c - '0');
+            }
+            else if(c == '+'){
+                result += sign * number;
+                number = 0;
+                sign = 1;
+            }
+            else if(c == '-'){
+                result += sign * number;
+                number = 0;
+                sign = -1;
+            }
+            else if(c == '('){
+                stack.push(result);
+                stack.push(sign);
+                number = 0;
+                result = 0;
+                sign = 1;
+            }
+            else if(c == ')'){
+                result += sign * number;
+                number = 0;
+                result *= stack.pop();
+                result += stack.pop();
+            }
+        }
+        if(number != 0){
+            result += sign * number;
+        }
+        return result;
+    }
+}
