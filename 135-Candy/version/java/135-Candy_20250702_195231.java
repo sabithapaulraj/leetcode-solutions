5afcec965bb473785430f56f3eb09cc55d8061e0
Sabitha Paulraj <118343379+sabithapaulraj@users.noreply.github.com>
2025-07-02 19:52:31 +0530

    Create 135-Candy

---
+++ 135-Candy/version/java/135-Candy_20250702_195231.java
@@ -0,0 +1,29 @@
+// Last updated: 7/2/2025, 7:52:31 PM
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1); // Give 1 candy to everyone
+
+        // First pass: left to right
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        // Second pass: right to left
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        // Sum up all candies
+        int total = 0;
+        for (int c : candies){
+             total += c;
+        }
+        return total;
+    }
+}
