461d2b6c15f39ec48782c3fb2d870423d34f4fa6
Sabitha Paulraj <118343379+sabithapaulraj@users.noreply.github.com>
2025-07-08 22:27:53 +0530

    Create 349-IntersectionofTwoArrays

---
+++ 349-IntersectionofTwoArrays/version/java/349-IntersectionofTwoArrays_20250708_222754.java
@@ -0,0 +1,27 @@
+// Last updated: 7/8/2025, 10:27:54 PM
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        
+        List<Integer> result = new ArrayList<>();
+        int i = 0, j = 0;
+        
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                if (result.isEmpty() || result.get(result.size() - 1) != nums1[i]) {
+                    result.add(nums1[i]);
+                }
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        
+        return result.stream().mapToInt(k -> k).toArray();
+        
+    }
+}
