02b936e6b112f0f410777f9f5b392c38e8bc70f5
Sabitha Paulraj <118343379+sabithapaulraj@users.noreply.github.com>
2025-07-09 10:49:35 +0530

    Create 560-SubarraySumEqualsK

---
+++ 560-SubarraySumEqualsK/version/java/560-SubarraySumEqualsK_20250709_104935.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2025, 10:49:35 AM
+class Solution{
+    public int subarraySum(int[] nums, int k) {
+    int count = 0;
+    for (int i = 0; i < nums.length; i++) {
+        int sum = 0;
+        for (int j = i; j < nums.length; j++) {
+            sum += nums[j];
+            if (sum == k) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+}
