8192bd2bedc590227aebff082460ca2c3795cd3f
Sabitha Paulraj <118343379+sabithapaulraj@users.noreply.github.com>
2025-07-12 23:13:38 +0530

    Create 1346-CheckIfNandItsDoubleExist

---
+++ 1346-CheckIfNandItsDoubleExist/version/java/1346-CheckIfNandItsDoubleExist_20250712_231338.java
@@ -0,0 +1,14 @@
+// Last updated: 7/12/2025, 11:13:38 PM
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+       Set<Integer> seen = new HashSet<>();
+        
+        for (int num : arr) {
+            if (seen.contains(num * 2) || (num % 2 == 0 && seen.contains(num / 2))) {
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
+}
