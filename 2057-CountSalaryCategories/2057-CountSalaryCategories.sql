fc32552b87cde00ca56df318f904c3926bcba614
Sabitha Paulraj <118343379+sabithapaulraj@users.noreply.github.com>
2025-08-17 20:39:00 +0530

    Create 2057-CountSalaryCategories

---
+++ 2057-CountSalaryCategories/2057-CountSalaryCategories.sql
@@ -0,0 +1,16 @@
+-- Last updated: 8/17/2025, 8:39:00 PM
+SELECT "Low Salary" AS category,
+       sum(income < 20000) AS accounts_count
+  FROM Accounts
+
+UNION
+
+SELECT "Average Salary" AS category,
+       sum(income BETWEEN 20000 AND 50000) AS accounts_count
+  FROM Accounts
+
+UNION
+
+SELECT "High Salary" AS category,
+       sum(income > 50000) AS accounts_count
+  FROM Accounts;
