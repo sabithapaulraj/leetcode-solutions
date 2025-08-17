e212ed724427d92966b6f6be326917ef917ddacc
Sabitha Paulraj <118343379+sabithapaulraj@users.noreply.github.com>
2025-08-17 20:38:58 +0530

    Create 2087-ConfirmationRate

---
+++ 2087-ConfirmationRate/2087-ConfirmationRate.sql
@@ -0,0 +1,3 @@
+-- Last updated: 8/17/2025, 8:38:58 PM
+select s.user_id, round(avg(if(c.action="confirmed",1,0)),2) as confirmation_rate
+from Signups as s left join Confirmations as c on s.user_id= c.user_id group by user_id;
