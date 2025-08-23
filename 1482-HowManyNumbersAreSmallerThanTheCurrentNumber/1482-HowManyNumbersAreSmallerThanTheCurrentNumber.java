173fd0e5f90a269eacf9ec9a9cc2f7048d6e2406
Sabitha Paulraj <118343379+sabithapaulraj@users.noreply.github.com>
2025-08-23 18:49:55 +0530

    Create 1482-HowManyNumbersAreSmallerThanTheCurrentNumber

---
+++ 1482-HowManyNumbersAreSmallerThanTheCurrentNumber/1482-HowManyNumbersAreSmallerThanTheCurrentNumber.java
@@ -0,0 +1,26 @@
+// Last updated: 8/23/2025, 6:49:54 PM
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        // Optimal Solution - Frequency Counting Method
+        int[] count = new int[101];
+        // Calculate Frequnecy of each number
+        for(int num:nums){
+            count[num]++;
+        }
+        // Impelemnt Prefix - Sum Logic
+        for(int i = 1; i < 101; i++){
+            count[i] += count[i-1];
+        }
+        // Output Array
+        int[] result = new int[nums.length];
+        for(int i = 0; i< nums.length;i++){
+            if(nums[i] == 0){
+                result[i] = 0;
+            }
+            else{
+            result[i] = count[nums[i] - 1];
+            }
+        }
+        return result;
+    }
+}
