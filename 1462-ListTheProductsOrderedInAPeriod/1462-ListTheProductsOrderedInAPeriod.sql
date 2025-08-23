b0a7daf250486d9f06006d37cfc18f549d49b9b9
Sabitha Paulraj <118343379+sabithapaulraj@users.noreply.github.com>
2025-08-23 18:49:56 +0530

    Create 1462-ListTheProductsOrderedInAPeriod

---
+++ 1462-ListTheProductsOrderedInAPeriod/1462-ListTheProductsOrderedInAPeriod.sql
@@ -0,0 +1,14 @@
+-- Last updated: 8/23/2025, 6:49:55 PM
+SELECT
+    p.product_name,
+    SUM(o.unit) AS unit
+FROM
+    Products p
+JOIN
+    Orders o ON p.product_id = o.product_id
+WHERE
+    o.order_date BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY
+    p.product_name
+HAVING
+    SUM(o.unit) >= 100;
