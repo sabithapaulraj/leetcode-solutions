f8e43f125e897123f39bd5253d30b60114392656
Sabitha Paulraj <118343379+sabithapaulraj@users.noreply.github.com>
2025-09-02 19:52:52 +0530

    File updated at 9/2/2025, 7:52:54 PM

--- 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
+++ 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
@@ -1,19 +1,24 @@
-// Last updated: 9/2/2025, 7:51:20 PM
+// Last updated: 9/2/2025, 7:52:54 PM
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n = nums1.length;
         int m = nums2.length;
         int merged[] = new int[n + m];
+        // copy of arr1
         for(int i = 0; i < n; i++){
             merged[i] = nums1[i];
         }
+        //copy of arr2 added to arr1
         for(int j = 0; j < m; j++){
             merged[n + j] = nums2[j];
         }
+        //sort new arr
         Arrays.sort(merged);
+        //if merged arr odd length - mid element is median
         if(merged.length % 2 != 0){
             return merged[merged.length/2];
         }
+        // if merged arr even length - sum of mid element and previous element / 2 is median
         else{
             double mid1 = merged[merged.length/2];
             double mid2 = merged[merged.length/2 - 1];
