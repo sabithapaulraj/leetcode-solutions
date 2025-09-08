8d214b4fb77e8c356ffeb12ccef68d4090772510
Sabitha Paulraj <118343379+sabithapaulraj@users.noreply.github.com>
2025-09-08 15:45:09 +0530

    File updated at 9/8/2025, 3:45:10 PM

--- 74-Searcha2DMatrix/74-Searcha2DMatrix.java
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.java
@@ -1,17 +1,17 @@
-// Last updated: 9/8/2025, 11:33:42 AM
+// Last updated: 9/8/2025, 3:45:10 PM
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        if (matrix == null || m == 0 || n == 0){
+        int m = matrix.length; // no of rows
+        int n = matrix[0].length; // no of columns
+        if (matrix == null || m == 0 || n == 0){ //base case
             return false;
         }
         int left = 0;
         int right = m * n - 1;
-        while(left <= right){
+        while(left <= right){ //two - pointer approach
             int mid = left + (right - left) / 2;
-            int row = mid / n;
-            int column = mid % n;
+            int row = mid / n; // row of mid
+            int column = mid % n; // column of mid
             if(matrix[row][column] == target){
                 return true;
             }
