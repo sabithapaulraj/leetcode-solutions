25d0310e4b3887126665dc7816cf153a2307e7c6
Sabitha Paulraj <118343379+sabithapaulraj@users.noreply.github.com>
2025-09-08 10:37:03 +0530

    File updated at 9/8/2025, 10:37:03 AM

--- 1317-ConvertIntegertotheSumofTwoNo-ZeroIntegers/1317-ConvertIntegertotheSumofTwoNo-ZeroIntegers.java
+++ 1317-ConvertIntegertotheSumofTwoNo-ZeroIntegers/1317-ConvertIntegertotheSumofTwoNo-ZeroIntegers.java
@@ -1,15 +1,26 @@
-// Last updated: 9/8/2025, 10:29:27 AM
+// Last updated: 9/8/2025, 10:37:03 AM
 class Solution {
     public int[] getNoZeroIntegers(int n) {
-        for(int a = 1; a < n; a++){
-            int b = n - a;
+        for(int i = 0; i < n; i++){
+            int a = i; 
+            int b = n - i;
             int[] arr = new int[2];
-            if(!String.valueOf(a).contains("0") && !String.valueOf(b).contains("0")){
+            if((a > 0) && (b > 0) && (hasZero(a) == false) && (hasZero(b) == false)){
                 arr[0] = a;
                 arr[1] = b;
                 return arr;
             }
         }
         return new int[]{};
+    }
+    public boolean hasZero(int n){
+        while(n > 0){
+            int digit = n % 10;
+            if(digit == 0){
+                return true;
+            }
+            n = n / 10;
+        }
+        return false;
     }
 }
